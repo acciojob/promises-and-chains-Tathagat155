c1b83971fb3d70130626a3e1deb30ed4b86250c3
Tathagat155 <tathagatkumar70618@gmail.com>
2025-12-04 10:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const name=document.getElementById("name");
 	  }
       const promise=new Promise((resolve,reject)=>{
 		  if(parseInt(age.value)>18){
-			 setTimeout(()=>resolve("Welcome, . You can vote"),4000);
+			 setTimeout(()=>resolve(`Welcome,${name.value} You can vote`),4000);
 		  }else{
-             setTimeout(()=>reject("Oh sorry . You aren't old enough."),4000);
+             setTimeout(()=>reject(`Oh sorry ${name.value} You aren't old enough.`),4000);
 		  }
 	  }).then((data)=>alert(data)).catch((err)=>alert(err));
 	  
